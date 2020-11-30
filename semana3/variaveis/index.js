9915c83d1441d73ebc770a7fc6902f63d3c2ccd1
PlatonicSkeptic <viniciusalves932@gmail.com>
2020-11-30 14:10:19 -0300

    exercicio js 1

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,76 @@
+
+// a = 10
+// b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+// sera impresso: 10 5
+
+// // a = 10
+// // b = 20
+// // c = a
+// // b = c
+// // a = b
+
+// // console.log(a, b, c)
+// sera impresso: 10 20 10
+
+
+// let nome;
+// let idade;
+// console.log (typeof (nome));
+// // o tipo undefined foi impresso pq a variavel nao foi declarada
+// nome = prompt("1. qual seu nome?")
+// console.log ("Resposta: ",nome,"")
+// idade = prompt("2. qual sua idade?")
+// console.log ("Resposta: ",idade,"")
+
+// console.log (typeof (nome, idade))
+// // STrING!! ;/
+// console.log ("Oi ",nome,", vc tem ",idade,"anos")
+
+
+// cor = prompt("3. qual sua cor favorita??")
+// console.log ("Resposta: ",cor,"")
+// endereço = prompt("4. qual seu endereço??")
+// console.log ("Resposta: ",endereço,"")
+// diagnostico = prompt("vc tem autismo??")
+// console.log ("Resposta: ",diagnostico,"")
+
+
+
+// food0 = ("brownie")
+// food1 = ("sorvete")
+// food2 = ("pizza de alface")
+// food4 = ("churrasco vegano")
+// food3 = ("proteina de ervilha")
+
+// let array = [food0, food1, food2, food3, food4]
+// console.log (food0)
+// console.log (food1)
+// console.log (food2)
+// console.log (food3)
+// console.log (food4)
+
+// array [1] = (prompt("Qual a sua comida favorita?"))
+// console.log (array[0])
+// console.log (array[1])
+// console.log (array[2])
+// console.log (array[3])
+// console.log (array[4])
+
+
+let Q = ["tu ta usando uma roupa azul hj?", "bah que legal, e ela é sua?", "vamo dança pelado no skype??"]
+let A = []
+A [0] = prompt(Q [0])
+A [1] = prompt(Q [1])
+A [2] = prompt(Q [2]) 
+Boolean(A [0])
+Boolean(A [1])
+Boolean(A [2])
+console.log (Q [0],A [0])
+console.log (Q [1],A [1])
+console.log (Q [2],A [2])
