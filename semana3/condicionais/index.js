a653ffbf5dae044bde47e51e94e71e290e5459b2
PlatonicSkeptic <viniciusalves932@gmail.com>
2020-12-02 12:17:37 -0300

    exercicios condicionais

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,54 @@
+// EX.1
+//  teste de numero par usando floor division
+
+// EX.2
+// a. serve para setar a variavel preco com o valor da fruta
+// b. será impresso: O preço da fruta Maçã é R$ 2.25
+// c. será impresso: O preço da fruta Pêra é R$ 5
+
+// EX.3
+// a. declarando uma constante tipo numero
+// b. nada
+// c. pq a variavel contendo a mensagem foi criada num escopo filho
+
+// // // //**Exercícios de escrita de código** // // //
+
+// EX.4
+// idade = Number(prompt("Qual sua idade??"))
+// if (idade >= 18)
+//     console.log("Você pode dirigir")
+// else
+//     console.log("Você não pode dirigir.")
+
+// EX.5
+// const turno = prompt("Qual turno voce estuda? Responda: M/V/N (Matutino/Vespertino/Noturno")
+
+// if (turno == "M")
+//         console.log("Bom Dia!")
+//     else if (turno == "V")
+//         console.log("Boa Tarde!")
+//     else if (turno == "V")
+//         console.log("Boa Noite!")
+// EX.6
+
+// turno = prompt("Qual turno voce estuda? Responda: M/V/N (Matutino/Vespertino/Noturno")
+// switch (turno){
+//     case "M":
+//     console.log("Bom Dia!")
+//     break
+//     case "V":
+//     console.log("Boa Tarde!")
+//     break
+//     case "N":
+//     console.log("Boa Noite!")
+//     break
+// }
+// EX.7
+
+tipo = prompt("qual o gênero de filme que vão assistir??")
+preco = Number(prompt("preço do ingresso?(R$)"))
+
+if (preco < 15 && tipo == "fantasia")
+    console.log("Bom filme!")
+    else
+    console.log("Escolha outro filme :(")
